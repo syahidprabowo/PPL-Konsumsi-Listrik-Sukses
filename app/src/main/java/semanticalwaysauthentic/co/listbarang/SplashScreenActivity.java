3bf46b17b34a2e88194fc4c4da08a54aac755bbf
alfincahyo <38088162+alfincahyo@users.noreply.github.com>
2019-04-07 13:01:13 +0700

    Add files via upload

---
+++ app/src/main/java/semanticalwaysauthentic/co/listbarang/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package semanticalwaysauthentic.co.listbarang;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, HomeScreenActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
