944fa57c5458c6aaaa9748e565d1bdff1927adaf
alfincahyo <38088162+alfincahyo@users.noreply.github.com>
2019-04-07 12:54:48 +0700

    Update open helper

--- app/src/main/java/semanticalwaysauthentic/co/listbarang/ListBarangOpenHelper.java
+++ app/src/main/java/semanticalwaysauthentic/co/listbarang/ListBarangOpenHelper.java
@@ -13,13 +13,16 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
     private static final String TAG = ListBarangOpenHelper.class.getSimpleName();
     // has to be 1 first time or app will crash
     private static final int DATABASE_VERSION = 1;
-    private static final String WORD_LIST_TABLE = "Nama_Barang";
+    private static final String TABEL_BARANG = "Nama_Barang";
     private static final String DATABASE_NAME = "ListBarang";
     // Column names...
     public static final String KEY_ID = "_id";
-    public static final String KEY_WORD = "word";
+    public static final String KEY_WORD = "barang";
+    public static final String KEY_WATT = "watt";
+    public static final String KEY_DURASI = "durasi";
+    public static final String KEY_BIAYA = "biaya";
     // ... and a string array of columns.
-    private static final String[] COLUMNS = { KEY_ID, KEY_WORD };
+    private static final String[] COLUMNS = { KEY_ID, KEY_WORD, KEY_WATT,KEY_DURASI, KEY_BIAYA};
 
     private SQLiteDatabase mWritableDB;
     private SQLiteDatabase mReadableDB;
@@ -29,10 +32,11 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
 
     // Build the SQL query that creates the table.
     private static final String WORD_LIST_TABLE_CREATE =
-            "CREATE TABLE " + WORD_LIST_TABLE + " (" +
+            "CREATE TABLE " + TABEL_BARANG + " (" +
                     KEY_ID + " INTEGER PRIMARY KEY, " +
 // id will auto-increment if no value passed
-                    KEY_WORD + " TEXT );";
+                    KEY_WORD + " TEXT, "+ KEY_WATT + " INTEGER , "+ KEY_DURASI +
+                    " INTEGER," + KEY_BIAYA + " FLOAT );";
 
     @Override
     public void onCreate(SQLiteDatabase db) {
@@ -41,16 +45,23 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
     }
 
     private void fillDatabaseWithData(SQLiteDatabase db) {
-        String[] words = {"Kipas Angin", "Air Conditioner", "Proyektor",
-                "Lampu","Laptop", "Komputer", "Televisi",
-                "Router", "Sound System","Lift",
-                "Layar LCD"};
+        String[] nama_barang = {"AC 1 PK", "AC 1/2 PK", "AC 2 PK",
+                "Lampu TL LED", "Komputer PC", "Proyektor",
+                "Kipas Angin"};
 
-        for (int i=0; i < words.length; i++) {
+        Integer[] watt_barang = {840, 1170, 1920, 16, 140, 205, 103};
+
+        Integer[] durasi = {2, 1, 6, 12, 2, 5, 7};
+
+        for (int i=0; i < nama_barang.length; i++) {
             // Put column/value pairs into the container.
             // put() overrides existing values.
-            values.put(KEY_WORD, words[i]);
-            db.insert(WORD_LIST_TABLE, null, values);
+            double biaya = HitungBiaya(durasi[i], watt_barang[i]);
+            values.put(KEY_WORD, nama_barang[i]);
+            values.put(KEY_WATT, watt_barang[i]);
+            values.put(KEY_DURASI, durasi[i]);
+            values.put(KEY_BIAYA, biaya);
+            db.insert(TABEL_BARANG, null, values);
         }
     }
 
@@ -60,7 +71,7 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
         Log.w(ListBarangOpenHelper.class.getName(),
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
-        db.execSQL("DROP TABLE IF EXISTS " + WORD_LIST_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABEL_BARANG);
         onCreate(db);
 
     }
@@ -70,8 +81,8 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
     }
 
     public itembarang query(int position){
-        String query = "SELECT  * FROM " + WORD_LIST_TABLE +
-                " ORDER BY " + KEY_WORD + " ASC " +
+        String query = "SELECT  * FROM " + TABEL_BARANG +
+                " ORDER BY " + KEY_ID + " ASC " +
                 "LIMIT " + position + ",1";
 
         Cursor cursor = null;
@@ -82,6 +93,9 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
             cursor.moveToFirst();
             entry.setId(cursor.getInt(cursor.getColumnIndex(KEY_ID)));
             entry.setWord(cursor.getString(cursor.getColumnIndex(KEY_WORD)));
+            entry.setWatt(cursor.getInt(cursor.getColumnIndex(KEY_WATT)));
+            entry.setmDurasi(cursor.getInt(cursor.getColumnIndex(KEY_DURASI)));
+            entry.setmBiaya(cursor.getInt(cursor.getColumnIndex(KEY_BIAYA)));
 
         }catch (Exception e){
 
@@ -96,15 +110,18 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
         }
     }
 
-    public long insert(String barang){
+    public long insert(String barang, int watt, int durasi){
         long newId = 0;
         ContentValues values = new ContentValues();
         values.put(KEY_WORD, barang);
+        values.put(KEY_WATT, watt);
+        values.put(KEY_DURASI, durasi);
+        values.put(KEY_BIAYA, HitungBiaya(durasi, watt));
         try{
             if (mWritableDB == null){
                 mWritableDB = getWritableDatabase();
             }
-            newId = mWritableDB.insert(WORD_LIST_TABLE, null, values);
+            newId = mWritableDB.insert(TABEL_BARANG, null, values);
         }catch (Exception e){
             Log.d(TAG, "GAGAL INSERT !" + e.getMessage());
         }
@@ -115,7 +132,7 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
         if (mReadableDB == null) {
             mReadableDB = getReadableDatabase();
         }
-        return DatabaseUtils.queryNumEntries(mReadableDB, WORD_LIST_TABLE);
+        return DatabaseUtils.queryNumEntries(mReadableDB, TABEL_BARANG);
     }
 
     public int delete(int id){
@@ -124,7 +141,7 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
             if (mWritableDB == null){
                 mWritableDB = getWritableDatabase();
             }
-            deleted = mWritableDB.delete(WORD_LIST_TABLE, //nama barang
+            deleted = mWritableDB.delete(TABEL_BARANG, //nama barang
             KEY_ID + "=?", new String[]{String.valueOf(id)});
         }catch (Exception e){
             Log.d(TAG, "DELETE EXCEPTION"+e.getMessage());
@@ -132,7 +149,7 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
         return deleted;
     }
 
-    public int update(int id, String word) {
+    public int update(int id, String word, int watt, int durasi) {
         int mNumberOfRowsUpdated = -1;
         try {
             if (mWritableDB == null) {
@@ -140,7 +157,10 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
             }
             ContentValues values = new ContentValues();
             values.put(KEY_WORD, word);
-            mNumberOfRowsUpdated = mWritableDB.update(WORD_LIST_TABLE,
+            values.put(KEY_WATT, watt);
+            values.put(KEY_DURASI, durasi);
+            values.put(KEY_BIAYA, HitungBiaya(durasi, watt));
+            mNumberOfRowsUpdated = mWritableDB.update(TABEL_BARANG,
                     values,
                     KEY_ID + " = ?",
                     new String[]{String.valueOf(id)});
@@ -148,6 +168,14 @@ public class ListBarangOpenHelper extends SQLiteOpenHelper {
             Log.d (TAG, "UPDATE EXCEPTION! " + e.getMessage());
         }
         return mNumberOfRowsUpdated;
+    }
+
+    private double HitungBiaya(int durasi, int watt){
+
+        double biaya;
+        biaya =((watt*0.001) * 1632) * durasi;
+
+        return biaya;
     }
 
 }
