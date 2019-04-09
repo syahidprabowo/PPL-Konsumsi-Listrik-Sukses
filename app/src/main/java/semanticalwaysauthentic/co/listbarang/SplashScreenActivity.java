aa77cf434e13a192133388a67957b8214f20b4db
osafaridasari <37997955+osafaridasari@users.noreply.github.com>
2019-04-09 10:35:48 +0700

    Add files via upload

--- app/src/main/java/semanticalwaysauthentic/co/listbarang/SplashScreenActivity.java
+++ app/src/main/java/semanticalwaysauthentic/co/listbarang/SplashScreenActivity.java
@@ -15,7 +15,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this, HomeScreenActivity.class);
+                Intent i = new Intent(SplashScreenActivity.this, PilihKwH.class);
                 startActivity(i);
                 finish();
             }
